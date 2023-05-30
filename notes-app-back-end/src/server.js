f522b3f3586a984808138470f14a428daef299f2
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-05-30 11:45:27 +0700

    setting up

---
+++ notes-app-back-end/src/server.js
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+const Hapi = require('@hapi/hapi');
+
+async function init() {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
