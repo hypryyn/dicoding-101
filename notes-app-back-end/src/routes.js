52639c38351c54505582cf784788f32ac7df8bc2
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-05-30 16:42:41 +0700

    cors handling hapi

--- notes-app-back-end/src/routes.js
+++ notes-app-back-end/src/routes.js
@@ -1,9 +1,11 @@
 /* eslint-disable linebreak-style */
+const { addNoteHandler } = require('./handler');
+
 const routes = [
   {
     method: 'POST',
     path: '/notes',
-    handler: () => { },
+    handler: addNoteHandler,
   },
 ];
 
